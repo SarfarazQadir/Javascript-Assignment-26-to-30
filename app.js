66c78daf708222affee3eee7b5d0bf73a736bb24
Sarfaraz Qadir <sarfarazqadir594@gmail.com.com>
2024-10-03 09:21:43 +0500

    1

---
+++ app.js
@@ -0,0 +1,42 @@
+//  Assignment 26 to 30
+
+//  1
+
+// var user = +prompt();
+
+// document.write("number",user);
+// document.write("round of value : ",Math.round(user));
+// document.write("FLOOR of value : ",Math.floor(user));
+// document.write("ciel of value : ",Math.ceil(user));
+
+//  2
+
+//  var user = -prompt();
+
+// document.write("number",user);
+// document.write("<br>round of value : ",Math.round(user));
+// document.write("<br>FLOOR of value : ",Math.floor(user));
+// document.write("<br>ciel of value : ",Math.ceil(user));
+
+
+// 3
+//7
+
+var user = prompt("type your wieght");
+
+document.write(user.toFix);
+document.write(Math.round(user),"kgs");
+document.write(Math.floor(user),"kgs");
+document.write(Math.ceil(user),"kgs");
+
+//8
+
+// var sec = 10;
+
+// var user = prompt("guess the secret number");
+
+// if(user == sec){
+//     alert("congratulation" +sec+ " you guessed it");
+// }else{
+//     alert("sorry you guessed it wrong");
+// }
